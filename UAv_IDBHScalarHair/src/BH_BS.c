ee2acc6cb5be3b7ecbd4a3b35d9a134b6bdb3cb4
marcobrito98 <marcobrito@ua.pt>
2024-11-18 14:32:48 +0000

    fdd

--- UAv_IDBHScalarHair/src/BH_BS.c
+++ UAv_IDBHScalarHair/src/BH_BS.c
@@ -673,12 +673,12 @@ void UAv_ID_BH_BS(CCTK_ARGUMENTS)
 
 
         //Superposition (boosted black hole x direction, non spinning star at least)
-        gxx[ind] = pow(psi1_1+psi1_2-1,4)*gamma2*(1-bh_v2*pow(3-psi1_1-psi1_2,2)*pow(psi1_1+psi1_2-1,-6));
+        gxx[ind] = pow(psi1_2,4)*gamma2*(1-bh_v2*pow(2-psi1_2,2)*pow(psi1_2-1,-6));
         gxy[ind] = 0.;
         gxz[ind] = 0.;
-        gyy[ind] = pow(psi1_1+psi1_2-1,4); //o que temos aqui efetivamente e a sobreposicao de fatores conformes
+        gyy[ind] = pow(psi1_2,4); //o que temos aqui efetivamente e a sobreposicao de fatores conformes
         gyz[ind] = 0.;
-        gzz[ind] = pow(psi1_1+psi1_2-1,4);
+        gzz[ind] = pow(psi1_2,4);
 
 
         // extrinsic curvature (this will be zero due to W=0, at least the BS part)

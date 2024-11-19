c58d54bd813be662fa7cc20f574d26013f3d81d1
marcobrito98 <marcobrito@ua.pt>
2024-11-19 11:56:27 +0000

    test

--- UAv_IDBHScalarHair/src/BH_BS.c
+++ UAv_IDBHScalarHair/src/BH_BS.c
@@ -621,7 +621,7 @@ void UAv_ID_BH_BS(CCTK_ARGUMENTS)
         const CCTK_REAL y1_2  = y[ind] - y0_2;
         const CCTK_REAL z1_2  = z[ind] - z0_2;
 
-        const CCTK_REAL bh_v = 1.0;
+        const CCTK_REAL bh_v = 0.999;
         const CCTK_REAL bh_v2 = bh_v * bh_v;
         const CCTK_REAL gamma2 = 1. / (1. - bh_v2);
         const CCTK_REAL rr2_2 = x1_2*x1_2*gamma2 + y1_2*y1_2 + z1_2*z1_2;

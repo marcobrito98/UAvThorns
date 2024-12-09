4c8705dbd043c9de601aa0603393a793b2be95b5
marcobrito98 <marcobrito@ua.pt>
2024-12-09 11:09:55 +0000

    sdsdas

--- UAv_IDBHScalarHair/src/Kerr_BS.c
+++ UAv_IDBHScalarHair/src/Kerr_BS.c
@@ -579,7 +579,7 @@ void UAv_ID_Kerr_BS(CCTK_ARGUMENTS)
 
         const CCTK_REAL bh_v2 = bh_v * bh_v;
         const CCTK_REAL gamma2 = 1. / (1. - bh_v2);
-        const CCTK_REAL rr2_2 = x1_2*x1_2*gamma2 + y1_2*y1_2 + z1_2*z1_2;
+        const CCTK_REAL rr2_2 = x1_2*x1_2 + y1_2*y1_2 + z1_2*z1_2;
         const CCTK_REAL rr_2  = sqrt(rr2_2);
 
         // const CCTK_REAL rho2_2 = x1_2*x1_2 + y1_2*y1_2;

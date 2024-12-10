23da812e58be162a2ab6c4d80ef52b8c65647c79
marcobrito98 <marcobrito@ua.pt>
2024-12-10 11:06:28 +0000

    test3

--- UAv_IDBHScalarHair/src/Kerr_BS.c
+++ UAv_IDBHScalarHair/src/Kerr_BS.c
@@ -716,7 +716,7 @@ void UAv_ID_Kerr_BS(CCTK_ARGUMENTS)
         // 3-metric
         gxx[ind] = psi4_2 * ( 1. + bh_spin*bh_spin * hh * y1_2*y1_2 ) ;//+2*gamma2*bh_v*bh_spin*sigma*y1_2/rr2_2 + \
         gamma2*bh_v2*(-alpha02 + pow(bh_spin*sigma*sinth2,2))/(rr2_2*sinth2*(1+bh_spin*bh_spin*hh*hh*rr2_2*sinth2));
-        gxy[ind] = - gamma2*psi4_2 * bh_spin*bh_spin * hh * x1_2*y1_2;// - gamma2*bh_v*bh_spin*sigma*x1_2/rr2_2;
+        gxy[ind] = - gamma2*psi4_2 * bh_spin*bh_spin * hh * x1_2*y1_2 - gamma2*bh_v*bh_spin*sigma*x1_2/rr2_2;
         gxz[ind] = 0;
         gyy[ind] = psi4_2 * ( 1. + bh_spin*bh_spin * hh * gamma2*x1_2*x1_2 );
         gyz[ind] = 0;

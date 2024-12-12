6ca0043fa85c7576cf6aa3a657cfd36d10408b8b
marcobrito98 <marcobrito@ua.pt>
2024-12-12 14:44:02 +0000

    dasd

--- UAv_IDBHScalarHair/src/Kerr_BS.c
+++ UAv_IDBHScalarHair/src/Kerr_BS.c
@@ -667,10 +667,10 @@ void UAv_ID_Kerr_BS(CCTK_ARGUMENTS)
         const CCTK_REAL dbetauphi_dR = (gammaphiphi*dbetadphi_dR - betadphi*dgammaphiphi_dR)/pow(betadphi,2);//podem ser estes que estejam a dar erro.
         // printf("%.6f",dbetauphi_dth);
         // printf("%.6f",dbetauphi_dR);//nao parece haver nans
-        check_nan("betauphi",betauphi)
-        check_nan("betauphi",betadphi)
-        check_nan("betauphi",dbetauphi_dR)
-        check_nan("betauphi",dbetauphi_dth)
+        check_nan("betauphi",betauphi);
+        check_nan("betauphi",betadphi);
+        check_nan("betauphi",dbetauphi_dR);
+        check_nan("betauphi",dbetauphi_dth);
 
 
         //capital Ks refer to the unboosted frame.

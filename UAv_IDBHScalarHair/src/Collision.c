76a041ef8d24bb75ec7d204be3dd00a9d11c4dcc
marcobrito98 <marcobrito@ua.pt>
2025-01-16 11:09:13 +0000

    correction added to superposition

--- UAv_IDBHScalarHair/src/Collision.c
+++ UAv_IDBHScalarHair/src/Collision.c
@@ -747,12 +747,12 @@ printf("correction = %f \n",correction);
         CCTK_REAL gzz_2 = conf_fac_2;
 
         //Superposition
-        gxx[ind] = gxx_1 + gxx_2 - 1;
+        gxx[ind] = gxx_1 + gxx_2 - correction;
         gxy[ind] = gxy_1 + gxy_2;
         gxz[ind] = gxz_1 + gxz_2;
-        gyy[ind] = gyy_1 + gyy_2 - 1;
+        gyy[ind] = gyy_1 + gyy_2 - correction;
         gyz[ind] = gyz_1 + gyz_2;
-        gzz[ind] = gzz_1 + gzz_2 - 1;
+        gzz[ind] = gzz_1 + gzz_2 - correction;
 
 
 

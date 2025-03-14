49ac5e0937fb0f86859de2e4f79207e02cd3fb64
marcobrito98 <marcobrito@ua.pt>
2025-03-14 11:09:41 +0000

    fixes

--- UAv_IDBHScalarHair/src/Kerr_BS.c
+++ UAv_IDBHScalarHair/src/Kerr_BS.c
@@ -597,10 +597,10 @@ void UAv_ID_Kerr_BS(CCTK_ARGUMENTS)
         const CCTK_REAL y1_2     = y[ind] - y0_2;
         const CCTK_REAL z1_2     = z[ind] - z0_2;
 
-        const CCTK_REAL rr2_2    = x1_2*x1_2 + y1_2*y1_2 + z1_2*z1_2 ;
+        CCTK_REAL rr2_2    = x1_2*x1_2 + y1_2*y1_2 + z1_2*z1_2 ;
         if( rr2_2 < pow( eps_r, 2 ) ) 
         rr2_2 = pow( eps_r, 2 );
-        const CCTK_REAL rr_2     = sqrt(rr2_2) ;
+        CCTK_REAL rr_2     = sqrt(rr2_2) ;
 
         CCTK_REAL rho_2, rho2_2, rho3_2;
         rho2_2 = x1_2 * x1_2 + y1_2 * y1_2; 

210745be615fa77e30a112ce4d63585c47008161
marcobrito98 <marcobrito@ua.pt>
2025-06-25 14:35:44 +0100

    missing bracket

--- UAv_IDBHScalarHair/src/Kerr_BS.c
+++ UAv_IDBHScalarHair/src/Kerr_BS.c
@@ -1036,7 +1036,7 @@ void UAv_ID_Kerr_BS(CCTK_ARGUMENTS)
 
 
         CCTK_REAL new_lapse = sqrt(-g[0][0] + betad[1]*betaup[1] + betad[2]*betaup[2] + betad[3]*betaup[3]); 
-        if (new_lapse < SMALL)
+        if (new_lapse < SMALL){
             new_lapse = SMALL;
         }
         // if (fabs(new_lapse) < 1e-16) {

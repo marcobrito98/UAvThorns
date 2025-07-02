b1e986eb7d445f62b2404482d8e022acbc274db7
marcobrito98 <marcobrito@ua.pt>
2025-07-02 18:51:25 +0100

    test

--- UAv_IDBHScalarHair/src/Kerr_BS.c
+++ UAv_IDBHScalarHair/src/Kerr_BS.c
@@ -1125,6 +1125,7 @@ void UAv_ID_Kerr_BS(CCTK_ARGUMENTS)
         fprintf(stderr, "Error: %s is NaN\n", "new_lapse");
         fprintf(stderr, "g00 = %e \n", g[0][0]);
         fprintf(stderr, "beta2 = %e \n", betad[1]*betaup[1] + betad[2]*betaup[2] + betad[3]*betaup[3]);
+        fprintf(stderr, "Error: new_lapse is nan at grid point (%d,%d,%d)\n", i, j, k);
         abort(); // Break execution
         }
         

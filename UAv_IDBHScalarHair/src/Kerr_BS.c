0d5281293da2d37641942115bde5d02765a9c3c7
marcobrito98 <marcobrito@ua.pt>
2025-07-07 14:20:39 +0100

    dsads

--- UAv_IDBHScalarHair/src/Kerr_BS.c
+++ UAv_IDBHScalarHair/src/Kerr_BS.c
@@ -601,7 +601,7 @@ void UAv_ID_Kerr_BS(CCTK_ARGUMENTS)
         const CCTK_REAL bh_v2 = bh_v * bh_v;
         const CCTK_REAL gamma2 = 1. / (1. - bh_v2);
         const CCTK_REAL gamma = sqrt(gamma2);
-        printf("gamma = %e\n", gamma);
+        // printf("gamma = %e\n", gamma);
  
         CCTK_REAL rr2_2 = x1_2*x1_2*gamma2 + y1_2*y1_2 + z1_2*z1_2;
         if( rr2_2 < pow( eps_r, 2 ) ) {

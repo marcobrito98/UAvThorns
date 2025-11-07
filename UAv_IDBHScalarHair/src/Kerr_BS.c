706c7bf248e6808e29a33f7c0802c05356ba2b6c
marcobrito98 <marcobrito@ua.pt>
2025-11-07 14:33:51 +0000

    fixes

--- UAv_IDBHScalarHair/src/Kerr_BS.c
+++ UAv_IDBHScalarHair/src/Kerr_BS.c
@@ -691,8 +691,8 @@ void UAv_ID_Kerr_BS(CCTK_ARGUMENTS)
         fctGG = rBLm / (rr2_2 * (rBL - rBLm));
         dfctGG_dR = -((rBLm*(-2*rBLm + 2*rBL + rr_2*drBLdR))/(pow(rr_2,3)*pow(rBLm - rBL,2)));
         fctHH = (2.0 * bh_mass * rBL + Sigm) / (rr2_2 * Sigm2);
-        dfctHH_dR = (-2 * (bh_spin2 * costh2 + rBL2) * (bh_spin2 * costh2 + 2 * bh_mass * rBL + rBL2) - 2 * rr_2 * (-(bh_mass * bh_spin2 * costh2) + bh_spin2 * costh2 * rBL + 3 * bh_mass * rBL2 + pow(rBL, 3)) * drBLdR) / (pow(rr_2, 3) * pow(bh_spin2 * costh2 + rBL2, 3));
-        dfctHH_dth = (2 * bh_spin2 * costh * (bh_spin2 * costh2 + 4 * bh_mass * rBL + pow(rBL, 2)) * sinth) / (rr2_2 * pow(bh_spin2 * costh2 + rBL2, 3));
+        dfctHH_dR = -((2*Sigm*(2*bh_mass*rBL + Sigm - bh_mass*rr_2*drBLdR) + rr_2*(4*bh_mass*rBL + Sigm)*dSigm_dR)/(pow(rr_2,3)*pow(Sigm,3)));
+        dfctHH_dth = -(((4*bh_mass*rBL + Sigm)*dSigm_dth)/(rr2_2*pow(Sigm,3)));
         dpsi4_2_dR = (-2 * (bh_spin2 * costh2 + rBL2 - rr_2 * rBL * drBLdR)) / pow(rr_2, 3);
         dpsi4_2_dth = (-2 * bh_spin2 * costh * sinth) / rr2_2;
 
@@ -707,11 +707,11 @@ void UAv_ID_Kerr_BS(CCTK_ARGUMENTS)
         const CCTK_REAL alpha0 = (4.0 * rr_2 - rBLp) * sqrt(rBL - rBLm) / sqrt(16.0 * rr_2 * (rBL2 + bh_spin2 * (1.0 + 2.0 * bh_mass * rBL * sinth2 / Sigm))); //primeiro termo para schwarzschild e zero
         const CCTK_REAL alpha02 = alpha0 * alpha0;
         const CCTK_REAL bphiup = -2.0 * bh_mass * bh_spin * rBL / fctFF;
-        const CCTK_REAL bphi = bphiup * psi4_2 * sinth2;
+        const CCTK_REAL bphi = bphiup * (fctFF/Sigm) * sinth2;
         const CCTK_REAL dbphiup_dR = (2 * bh_mass * bh_spin * (-(fctFF * drBLdR) + rBL * dfctFF_dR)) / pow(fctFF, 2);
         const CCTK_REAL dbphiup_dth = (2 * bh_mass * bh_spin * rBL * dfctFF_dth) / pow(fctFF, 2);
-        const CCTK_REAL dbphi_dR = dbphiup_dR * psi4_2 * sinth2 + bphiup * dpsi4_2_dR * sinth2;
-        const CCTK_REAL dbphi_dth = dbphiup_dth * psi4_2 * sinth2 + bphiup * dpsi4_2_dth * sinth2 + bphiup * psi4_2 * 2.0 * sinth * costh;
+        const CCTK_REAL dbphi_dR = (sinth2*(bphiup*Sigm*dfctFF_dR + fctFF*(Sigm*dbphiup_dR - bphiup*dSigm_dR)))/sinth2;
+        const CCTK_REAL dbphi_dth = (sinth*(fctFF*Sigm*sinth*dbphiup_dth + bphiup*(Sigm*sinth*dfctFF_dth + fctFF*(2*costh*Sigm - sinth*dSigm_dth))))/Sigm2;
         const CCTK_REAL dalpha0_dR = 0.5 / alpha0 * (-(Delt * Sigm * dfctFF_dR) + fctFF * (Sigm * dDelt_dR + Delt * dSigm_dR)) / pow(fctFF, 2);
         const CCTK_REAL dalpha0_dth = 0.5 / alpha0 * (Delt * (-(Sigm * dfctFF_dth) + fctFF * dSigm_dth)) / pow(fctFF, 2);
 

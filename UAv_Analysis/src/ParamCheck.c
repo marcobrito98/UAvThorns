381f5f29ed151b058e17893db2703f204668862f
marcobrito98 <marcobrito@ua.pt>
2026-05-22 15:08:40 +0100

    added tracker

--- UAv_Analysis/src/ParamCheck.c
+++ UAv_Analysis/src/ParamCheck.c
@@ -10,32 +10,73 @@ void UAv_Analysis_ParamCheck(CCTK_ARGUMENTS){
   DECLARE_CCTK_ARGUMENTS;
   DECLARE_CCTK_PARAMETERS;
 
+  // -------------------------------
+  // MULTIPATCH
+  // -------------------------------
+  
+  // For information, see WARNING in param.ccl
+
+  // For multipatch / Llama grids, we need the volume form computed in Coordinates
+  // This is activated by the parameter Coordinates::store_volume_form = yes
+
+  // Trick to check multipatch usage without having to inherit the thorn
+  if( CCTK_IsFunctionAliased("MultiPatch_GetDomainSpecification") ) {
+    
+    // -1 is the "error" return value of ParameterGetType() (see repos/flesh/src/main/Parameters.c and repos/flesh/src/include/cctk_Parameter.h)
+    CCTK_INT type = -1; 
+    const CCTK_INT* store_volume_form_ptr = CCTK_ParameterGet("store_volume_form", "Coordinates", &type);
+
+    if (store_volume_form_ptr == NULL || type != PARAMETER_BOOLEAN) {
+      CCTK_ERROR("Problem acquiring pointer to Coordinates::store_volume_form parameter.");
+    }
+    else if (!(*store_volume_form_ptr)) {
+      CCTK_VPARAMWARN("You are using a multipatch system, but you set Coordinates::store_volume_form = no. "   
+                      "UAv_Analysis thorn needs to use the volume form for integrations. " 
+                      "Please set Coordinates::store_volume_form = yes.");
+    }
+    // else, we're good to go
+  }
+
+
+  // -------------------------------
+  // ORIGIN TRACKING
+  // -------------------------------
+
   // Check validity of grid scalars provided
   if (track_origin_from_grid_scalar) {
     CCTK_INT index;
+
     // x
     index = CCTK_VarIndex (track_origin_source_x);
-    if (index < 0) {
-      CCTK_VPARAMWARN("Could not get index of chosen track_origin_source_x: %s.", track_origin_source_x);
+    if (!CCTK_Equals(track_origin_source_x, "fixed")) {
+      if (index < 0) {
+        CCTK_VPARAMWARN("Could not get index of chosen track_origin_source_x: %s.", track_origin_source_x);
+      }
+      else if (CCTK_GroupTypeFromVarI(index) != CCTK_SCALAR) {
+        CCTK_VPARAMWARN("Chosen track_origin_source_x: %s is not a grid scalar.", track_origin_source_x);
+      }
     }
-    if (CCTK_GroupTypeFromVarI(index) != CCTK_SCALAR) {
-      CCTK_VPARAMWARN("Chosen track_origin_source_x: %s is not a grid scalar.", track_origin_source_x);
-    }
+    
     // y
     index = CCTK_VarIndex (track_origin_source_y);
-    if (index < 0) {
-      CCTK_VPARAMWARN("Could not get index of chosen track_origin_source_y: %s.", track_origin_source_y);
+    if (!CCTK_Equals(track_origin_source_y, "fixed")) {
+      if (index < 0) {
+        CCTK_VPARAMWARN("Could not get index of chosen track_origin_source_y: %s.", track_origin_source_y);
+      }
+      else if (CCTK_GroupTypeFromVarI(index) != CCTK_SCALAR) {
+        CCTK_VPARAMWARN("Chosen track_origin_source_y: %s is not a grid scalar.", track_origin_source_y);
+      }
     }
-    if (CCTK_GroupTypeFromVarI(index) != CCTK_SCALAR) {
-      CCTK_VPARAMWARN("Chosen track_origin_source_y: %s is not a grid scalar.", track_origin_source_y);
-    }
+    
     // z
     index = CCTK_VarIndex (track_origin_source_z);
-    if (index < 0) {
-      CCTK_VPARAMWARN("Could not get index of chosen track_origin_source_z: %s.", track_origin_source_z);
-    }
-    if (CCTK_GroupTypeFromVarI(index) != CCTK_SCALAR) {
-      CCTK_VPARAMWARN("Chosen track_origin_source_z: %s is not a grid scalar.", track_origin_source_z);
+    if (!CCTK_Equals(track_origin_source_z, "fixed")) {
+      if (index < 0) {
+        CCTK_VPARAMWARN("Could not get index of chosen track_origin_source_z: %s.", track_origin_source_z);
+      }
+      else if (CCTK_GroupTypeFromVarI(index) != CCTK_SCALAR) {
+        CCTK_VPARAMWARN("Chosen track_origin_source_z: %s is not a grid scalar.", track_origin_source_z);
+      }
     }
   }
 
--- UAv_Analysis/src/UAv_Initialization.c
+++ UAv_Analysis/src/UAv_Initialization.c
@@ -7,34 +7,95 @@ void UAv_Initialization (CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS;
   DECLARE_CCTK_PARAMETERS;
 
+
+  // -------------------------------
+  // MULTIPATCH
+  // -------------------------------
+
+  // For information, see WARNING in param.ccl
+
+  // Setting the flags has to be done here, cannot be done in PARAMCHECK
+
+  // Trick to check multipatch usage without having to inherit the thorn
+  *is_multipatch = CCTK_IsFunctionAliased("MultiPatch_GetDomainSpecification");
+  
+  // default (non Llama grid): don't use volume form
+  *use_volume_form = 0; 
+
+  // TODO: If there's ever a flag in Coordinates that indicates that the volume_form is trivial (e.g. is_cartesian),
+  // we could check it and set *use_volume_form = 0 in that case.
+
+  if (*is_multipatch > 0) {
+    // If the simulation uses multipatch, make sure that the volume form variable is stored.
+    // Supposedly, this should be handled correctly in Coordinates, 
+    // so this should be redundant with the ParamCheck on store_volume_form.
+    CCTK_INT* volume_form_state_ptr = CCTK_VarDataPtr(cctkGH, 0, "Coordinates::volume_form_state");
+    // CCTK_VINFO("volume_form_state = %d", *volume_form_state_ptr);
+    
+    if (volume_form_state_ptr != NULL) {
+      // No volume form actually stored (should not happen if ParamCheck passed)
+      if (*volume_form_state_ptr != 1) {
+        CCTK_ERROR("The patch system that you are using does not store the volume form, although you set Coordinates::store_volume_form = yes. "     
+                   "UAv_Analysis thorn will not be able to use the volume form for integrations. " 
+                   "There is likely a problem in thorn Coordinates.");
+      }
+      // else, we're good to go
+      else {
+        CCTK_INFO("Using volume form from multipatch system for integrations in UAv_Analysis.");
+        *use_volume_form = 1;
+      }
+    }
+    else {
+      CCTK_ERROR("Problem acquiring pointer to Coordinates::volume_form_state variable.");
+    }
+  }
+
+
+  // -------------------------------
+  // ORIGIN TRACKING
+  // -------------------------------
+
+  // We can already initialize the coordinates
+  *x0 = origin_x;
+  *y0 = origin_y;
+  *z0 = origin_z;
+
+  // Initialize origin tracking if needed
+  // origin_from_grid_scalar_index not allocated in schedule.ccl if track_origin_from_grid_scalar = no
   if (track_origin_from_grid_scalar) {
     // Get the index of variables. It's not supposed to change during the simulation (I think).
     // Validity of parameters should have been checked in ParamCheck.
-    // Seems a bit redundant to do this affectation here and not in ParamCheck, but more in the logic.
-    
+    // Since validity has been checked, a negative value here should mean "fixed".
+
     // x source
     *origin_from_grid_scalar_index_x = CCTK_VarIndex (track_origin_source_x);
     // y source
     *origin_from_grid_scalar_index_y = CCTK_VarIndex (track_origin_source_y);
     // z source
     *origin_from_grid_scalar_index_z = CCTK_VarIndex (track_origin_source_z);
-
-    CCTK_VINFO("Tracking origin used in the analysis with grid scalars.");
-    CCTK_VINFO("x0 = %s", track_origin_source_x);
-    CCTK_VINFO("y0 = %s", track_origin_source_y);
-    CCTK_VINFO("z0 = %s", track_origin_source_z);
   }
-  else { // no tracking from grid scalar
-    // origin_from_grid_scalar_index not allocated in schedule.ccl in that case
-
-    // We can already initialize the coordinates
-    *x0 = origin_x;
-    *y0 = origin_y;
-    *z0 = origin_z;
-
-    CCTK_VINFO("Using fixed origin in the analysis.");
-    CCTK_VINFO("x0 = %g", *x0);
-    CCTK_VINFO("y0 = %g", *y0);
-    CCTK_VINFO("z0 = %g", *z0);
+  
+  // Info
+  CCTK_VINFO("Origin used in the analysis:");
+  // x
+  if (track_origin_from_grid_scalar && *origin_from_grid_scalar_index_x >= 0) {
+    CCTK_VINFO("x0: %s", track_origin_source_x);
+  }
+  else {
+    CCTK_VINFO("x0 = %g (fixed)", *x0);
+  }
+  // y
+  if (track_origin_from_grid_scalar && *origin_from_grid_scalar_index_y >= 0) {
+    CCTK_VINFO("y0: %s", track_origin_source_y);
+  }
+  else {
+    CCTK_VINFO("y0 = %g (fixed)", *y0);
+  }
+  // z
+  if (track_origin_from_grid_scalar && *origin_from_grid_scalar_index_z >= 0) {
+    CCTK_VINFO("z0: %s", track_origin_source_z);
+  }
+  else {
+    CCTK_VINFO("z0 = %g (fixed)", *z0);
   }
 }
